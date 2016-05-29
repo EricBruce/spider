c258a8accd0bc38b182df2c2d8ff26c3230164d7
eric <eric.yxs@gmail.com>
2016-05-29 08:50:22 +0800

    add external config.properties

--- spider-furniture/src/main/java/eric/yxs/spider/furniture/common/Constant.java
+++ spider-furniture/src/main/java/eric/yxs/spider/furniture/common/Constant.java
@@ -1,17 +1,47 @@
 package eric.yxs.spider.furniture.common;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Properties;
+
 /**
  * Created by young on 16/5/28.
  */
+@Slf4j
 public class Constant {
 
+
+    private static Properties properties = new Properties();
+
+    static {
+
+        File file = new File("config.properties");
+        if (!file.exists()) {
+            log.warn("配置文件config.properties不存在");
+            System.exit(404);
+        }
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            properties.load(fis);
+            fis.close();
+        } catch (Exception e) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pr = new PrintWriter(sw);
+            e.printStackTrace(pr);
+            log.warn(sw.toString());
+        }
+    }
+
     public final static String urlPrefix = "http://www.markorhome.com";
-    public final static String basicPath = "/data/spider/furniture/";
-    public final static String schedulerCacheFile = "/data/spider/cache/";
+    public final static String basicPath = "" + properties.get("spider.data.base.path");
+    public final static String schedulerCacheFile = "" + properties.get("scheduler.cache.file.path");
 
     /* 美克美家 */
-    public final static String mkmjDomain = "http://www.markorhome.com/";
-    public final static String ethanAllenDomain = "http://www.ethanallen.com/";
-
+    public final static String mkmjDomain = "" + properties.get("spider.domain.mkmj");
+    public final static String ethanAllenDomain = "" + properties.get("spider.domain.ethanAllen");
 
 }
