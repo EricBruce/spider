52e680dcf8e4dc2ab359838d956c1a51f00aae12
eric <eric.yxs@gmail.com>
2016-05-29 08:20:15 +0800

    add spider monitor

--- spider-furniture/src/main/java/eric/yxs/spider/furniture/Application.java
+++ spider-furniture/src/main/java/eric/yxs/spider/furniture/Application.java
@@ -7,17 +7,24 @@ import eric.yxs.spider.furniture.common.model.EthanModel;
 import eric.yxs.spider.furniture.pipeline.CategoryPageModelPipeline;
 import eric.yxs.spider.furniture.pipeline.ChannelPageModelPipeline;
 import eric.yxs.spider.furniture.pipeline.EthanPageModelPipeline;
+import lombok.extern.slf4j.Slf4j;
 import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.model.OOSpider;
+import us.codecraft.webmagic.monitor.SpiderMonitor;
 import us.codecraft.webmagic.scheduler.FileCacheQueueScheduler;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 /**
  * Created by young on 16/5/28.
  */
+@Slf4j
 public class Application {
 
     public static void main(String[] args) {
-        OOSpider.create(Site.me().setSleepTime(1000)
+        Spider spider = OOSpider.create(Site.me().setSleepTime(1000)
                 .addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.63 Safari/537.36"))
                 .addPageModel(new ChannelPageModelPipeline(), ChannelModel.class)
                 .addPageModel(new CategoryPageModelPipeline(), CategoryModel.class)
@@ -25,7 +32,16 @@ public class Application {
                 .addUrl(Constant.mkmjDomain)
                 .addUrl(Constant.ethanAllenDomain)
                 .setScheduler((new FileCacheQueueScheduler(Constant.schedulerCacheFile)))
-                .thread(15)
-                .run();
+                .thread(15);
+        try {
+            SpiderMonitor.instance().register(spider);
+            spider.start();
+        } catch (Exception e) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pr = new PrintWriter(sw);
+            e.printStackTrace(pr);
+            log.warn(sw.toString());
+        }
+
     }
 }
